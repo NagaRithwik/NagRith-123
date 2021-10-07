ee67629ded24283ec25ea4c62e309aa95b283b26
NagaRithwik <89988578+NagaRithwik@users.noreply.github.com>
2021-10-07 18:47:58 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -52,14 +52,14 @@ var gameConfig={
 /*====================================
 =            Game Status             =
 ====================================*/
-
 noseX = "";
 noseY = "";
-gameStatus = "";
+GameStatus = "";
 
 function game(){
 
-  console.log("noseX=" + noseX + "noseY=" + noseY);
+  console.log("noseX = " + noseX +" ,noseY =  "+ noseY);
+
   instializeInDraw();
   moveEnvironment(mario);
   drawSprites();
@@ -72,7 +72,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Play Button to Start the Game ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Play Button To Start The Game ", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -89,13 +89,11 @@ function game(){
     scores(mario);
     manualControl(mario);
   
-    // optional control version of game
-    // autoControl(mario);
-  
   }
 
     // if game is over 
   if(gameConfig.status==='gameover'){
+    
     fill(0,0,0,150);
     rect(0,0,gameConfig.screenX,gameConfig.screenY);
 
@@ -116,21 +114,23 @@ function game(){
     changeGameStatud(mario)
   }
 }  
-function startGame(){
-  gameStatus = "start";
-  document.getElementById("status").innerHTML = "Game is Loading";
+
+function startGame()
+{
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
 }
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if(noseX !="" && gameConfig.status==="start" && gameStatus=="start") {
-    document.getElementById("status").innerHTML="Game is Loaded";
-    world_start.play();
-    initializeCharacterStatus(mario);
-    gameConfig.status= "play";
+ if(noseX !="" && gameConfig.status==="start" && GameStatus=="start") { 
+   document.getElementById("status").innerHTML = "Game Is Loaded";
+   world_start.play();
+ initializeCharacterStatus(mario)
+    gameConfig.status= "play"
   }
   if(gameConfig.status==="gameover" && keyDown(control.revive)) {
-    gameConfig.status= "start";        
+    gameConfig.status= "start"        
   }
 }
 
@@ -222,6 +222,7 @@ function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
     coin.get=true;
+    mario_coin.play();
   };
 }
     
@@ -295,7 +296,7 @@ function manualControl(character){
     }
 
     if(noseX > 300){
-      character.velocity.x+=gameConfig.moveSpeed;
+        character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
     }
@@ -309,8 +310,8 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (noseY < 200 &&character.live) || (touchIsDown&&character.live) ){
-		character.velocity.y+=gameConfig.jump;
+	if( (noseY < 168  &&character.live) || (touchIsDown&&character.live) ){
+    character.velocity.y+=gameConfig.jump;
     mario_jump.play();
 	}
 }
@@ -366,6 +367,7 @@ function StepOnEnemy(obj1,obj2){
     }else{
       obj1.velocity.y+=gameConfig.jump*0.8;
     }
+    mario_kick.play();
 	}
 }
 
@@ -378,6 +380,11 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+    console.log("die - " + character.liveNumber);
+    if(character.liveNumber > 0)
+    {
+      mario_die.play();
+    }
 }
 
 // check character status and response to sprite and game status
@@ -388,7 +395,8 @@ function checkStatus(character){
     reviveAfterMusic(character);
   }
   if(character.live==false && character.liveNumber==0){
-    gameConfig.status="gameover"
+    gameConfig.status="gameover";
+    mario_gameover.play();
   }
 
 }
@@ -410,7 +418,7 @@ function dontGetOutOfScreen(character){
   
   //if mario drop in the holes 
   if(character.position.y>gameConfig.screenY&&character.live && character==mario){
-  	die(mario);
+    die(mario);
   }
 
   if(character.position.x>gameConfig.screenX-(character.width*0.5)){
@@ -500,7 +508,7 @@ function moveEnvironment(character){
     environmentScrolling(platforms,environmentScrollingSpeed);
     environmentScrolling(bricks,environmentScrollingSpeed);
     environmentScrolling(clouds,environmentScrollingSpeed*0.5);
-    environmentScrolling(mountains,environmentScrollingSpeed*0.3); 
+    environmentScrolling(mountains,environmentScrollingSpeed*1.3); 
     environmentScrolling(pipes,environmentScrollingSpeed); 
     environmentScrolling(coins,environmentScrollingSpeed); 
     environmentScrolling(enemyMushrooms,environmentScrollingSpeed); 
@@ -621,5 +629,3 @@ function upSide(obj){ return obj.position.y-(obj.height/2);}
 function downSide(obj){ return obj.position.y+(obj.height/2);}
 
 /*=====  End of For Debugging  ======*/
-
-
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-nose = "";
-noseX = 0;
-noseY = 0;
-
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_game_over = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coins = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	setSprites();
 	MarioAnimation();
 }
@@ -31,7 +32,7 @@ function gotPoses(results){
 	if(results.length > 0){
 		console.log(results);
 		noseX = results[0].pose.nose.x;
-		noseY = results[0].pose.nose.noseY;
+		noseY = results[0].pose.nose.y;
 	}
 }
 
